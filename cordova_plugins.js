1c915cd88cc33f7efd97e6fa565a27bf63457d36
Joe Watkins <info@joe-watkins.io>
2013-11-12 10:39:30 -0800

    map page / kill status bar test

---
+++ cordova_plugins.js
@@ -0,0 +1,11 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    }
+]
+});
